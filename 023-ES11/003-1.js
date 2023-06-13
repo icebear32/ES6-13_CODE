15acf8a2fd6f52d67987ad152a0389bf9c31688e
icebear32 <1727693137@qq.com>
2023-06-13 22:54:05 +0800

    提交ES6-13代码

---
+++ 023-ES11/003-1.js
@@ -0,0 +1,9 @@
+console.log("1.js 加载了");
+
+export default {
+    name: "管理者模块"
+}
+
+export function test() {
+    console.log("1.js test");
+}
---
+++ 023-ES11/003-2.js
@@ -0,0 +1,5 @@
+console.log("2.js 加载了");
+
+export default {
+    name: "普通用户模块"
+}
